c87f51addbcb04ff5095b8ec3d06b50639bc1411
M-Asimm <kpkprince55persia@gmail.com>
2023-12-04 15:52:25 +0500

    some login setting in DB

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+require('./model/ConnectDB.js')();
 
 const app = express();
+app.use(cors())
 const PORT = 3000;
+
+// port listening
 app.listen(PORT, () => {
     console.log("connected to port " + PORT);
 })
-console.log("hello world!");
